57a26c46acef76aac8d46f0a17cdddfe68406bd4
Xijezu <xijezu@hotmail.de>
2017-04-09 11:35:11 +0200

    Initial Commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Diagnostics;
+using HANDLE = System.IntPtr;
+using System.Threading;
+
+namespace Infinite_Darkness_Launcher {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window {
+        #region For Starting
+        [DllImport( "Kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi, CharSet = CharSet.Auto )]
+        public static extern HANDLE CreateEvent(ref SECURITY_ATTRIBUTES lpEventAttributes, [In, MarshalAs( UnmanagedType.Bool )] bool bManualReset, [In, MarshalAs( UnmanagedType.Bool )] bool bIntialState, [In, MarshalAs( UnmanagedType.BStr )] string lpName);
+
+        [DllImport( "Kernel32.dll", SetLastError = true, CallingConvention = CallingConvention.Winapi )]
+        static extern UInt32 WaitForSingleObject(IntPtr hHandle, UInt32 dwMilliseconds);
+
+        [StructLayout( LayoutKind.Sequential )]
+        public struct SECURITY_ATTRIBUTES {
+            public int nLength;
+            public IntPtr lpSecurityDescriptor;
+            public int bInheritHandle;
+        }
+
+        static HANDLE createEvent() {
+            SECURITY_ATTRIBUTES securityAttrib = new SECURITY_ATTRIBUTES();
+
+            securityAttrib.bInheritHandle = 1;
+            securityAttrib.lpSecurityDescriptor = IntPtr.Zero;
+            securityAttrib.nLength = Marshal.SizeOf( typeof( SECURITY_ATTRIBUTES ) );
+
+            return CreateEvent( ref securityAttrib, false, false, String.Empty );
+        }
+        #endregion
+
+        private string ROOT_FOLDER = "";
+        private System.Collections.Generic.List<string> m_lLink = new System.Collections.Generic.List<string>();
+
+        private Thread oThread;
+        public static MainWindow m_pWnd;
+        public static bool isStartable = false;
+
+        public MainWindow() {
+            InitializeComponent();
+            ROOT_FOLDER = Path.GetDirectoryName( System.Reflection.Assembly.GetExecutingAssembly().Location );
+        }
+
+        private void imgClose_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+            Application.Current.Shutdown();
+        }
+
+        private void imgMinimize_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void btnStart_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) {
+            if ( isStartable ) {
+                ProcessStartInfo startInfo = new ProcessStartInfo();
+                HANDLE eventHandle = createEvent();
+
+                string sframeFilename = "SFrame.exe";
+                startInfo.EnvironmentVariables[String.Format( "{0}_PARENT", sframeFilename )] = "Launcher.exe";
+                startInfo.EnvironmentVariables[String.Format( "{0}_RUNNER", sframeFilename )] = eventHandle.ToString();
+                startInfo.UseShellExecute = false;
+                startInfo.FileName = "SFrame.exe";
+                startInfo.Arguments = " /auth_ip:auth.heavensfall.net /use_nprotect:0 /help_url_w:611 /help_url_h:625 /locale:ASCII /country:US /cash /commercial_shop /layout_dir:6 /layout_auto:0 /cash_url_w:800 /cash_url_h:631 /network.max_msg_process:1 /user_no:1";
+                try {
+                    Process.Start( startInfo );
+                }
+                catch ( Exception ex ) {
+                    MessageBox.Show( ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error );
+                    Application.Current.Shutdown();
+                }
+
+                WaitForSingleObject( eventHandle, 10 * 1000 );
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void Window_Initialized(object sender, EventArgs e) {
+            try {
+                CheckForOldFiles();
+                using ( System.Net.WebClient net = new System.Net.WebClient() ) {
+                    string[] t = net.DownloadString( "https://darknessfight.com/patch/patchnews.php" ).Split('\n');
+                    for(int i = 0; i < 6; i++)
+                    {
+                        var a = t[i].Split('%');
+                        m_lLink.Add(a[1]);
+                        a[0] = System.Net.WebUtility.HtmlDecode(a[0]);
+                        switch (i)
+                        {
+                            case 0:
+                                label_Copy1.Content = a[0];
+                                label_Copy1.MouseLeftButtonDown += openLink;
+                                break;
+                            case 1:
+                                label_Copy2.Content = a[0];
+                                label_Copy2.MouseLeftButtonDown += openLink;
+                                break;
+                            case 2:
+                                label_Copy3.Content = a[0];
+                                label_Copy3.MouseLeftButtonDown += openLink;
+                                break;
+                            case 3:
+                                label_Copy4.Content = a[0];
+                                label_Copy4.MouseLeftButtonDown += openLink;
+                                break;
+                            case 4:
+                                label_Copy5.Content = a[0];
+                                label_Copy5.MouseLeftButtonDown += openLink;
+                                break;
+                            case 5:
+                                label_Copy.Content = a[0];
+                                label_Copy.MouseLeftButtonDown += openLink;
+                                break;
+                        }
+                    }
+                }
+                    m_pWnd = this;
+                Patch patch = new Patch();
+                oThread = new Thread( new ThreadStart( patch.PatchClient ) ) { IsBackground = true };
+                oThread.Start();
+            }
+            catch ( Exception ex ) {
+                MessageBox.Show( ex.Message, "Error!", MessageBoxButton.OK, MessageBoxImage.Error );
+
+            }
+        }
+
+        private void openLink(object sender, MouseButtonEventArgs e)
+        {
+            string url = "";
+            if (sender == label_Copy1)
+                url = m_lLink[0];
+            else if (sender == label_Copy2)
+                url = m_lLink[1];
+            else if (sender == label_Copy3)
+                url = m_lLink[2];
+            else if (sender == label_Copy4)
+                url = m_lLink[3];
+            else if (sender == label_Copy5)
+                url = m_lLink[4];
+            else if (sender == label_Copy)
+                url = m_lLink[5];
+            Process.Start(url);
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            if ( oThread.IsAlive )
+                oThread.Abort();
+        }
+
+        private void CheckForOldFiles() {
+            string[] szFiles = { "Launcher.exe_OLD", "Launcher2.exe" };
+            foreach ( string szFile in szFiles ) {
+                string szExecutable = Path.Combine( ROOT_FOLDER, szFile );
+                if ( File.Exists( szExecutable ) ) {
+                    File.Delete( szExecutable );
+                }
+            }
+        }
+
+        private void btnSettings_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if(System.IO.File.Exists("RappelzConfig.exe"))
+            {
+                Process.Start("RappelzConfig.exe", "/locale:ASCII");
+            }
+            else
+            {
+                MessageBox.Show("You do not have a RappelzConfig.exe.");
+            }
+        }
+    }
+}
---
+++ Patch.cs
@@ -0,0 +1,242 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+using System.Windows;
+using System.Text;
+using System.Reflection;
+
+using ICSharpCode.SharpZipLib.Zip;
+
+namespace Infinite_Darkness_Launcher
+{
+    class Patch
+    {
+        private const string PATCH_SERVER = "https://darknessfight.com/patch/";
+        public string LAUNCHER_EXE = Assembly.GetExecutingAssembly().Location;
+        public string ROOT_FOLDER = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+        List<CSPATCH_INDEX> m_lPatchList = new List<CSPATCH_INDEX>();
+        private MainWindow m_pWnd;
+
+        private const int VERSION = 1103;
+        private int m_nCurrentVersion = 0;
+        private int m_nNewVersion = 0;
+
+        public void PatchClient() {
+            m_pWnd = MainWindow.m_pWnd;
+            try {
+                CheckForLauncherUpdate();
+                CheckForUpdates();
+            }
+            catch ( System.Net.WebException ex ) {
+                var result = MessageBox.Show( "There was an error connecting to the patch server.\nWould you like to try to connect to the game anyway?\n\n" + ex.Message, "Error!", MessageBoxButton.YesNo, MessageBoxImage.Question );
+                if ( result == MessageBoxResult.Yes ) {
+                    InvokeStartButton();
+                    InvokeUpdatePercentage( 100 );
+                    InvokeUpdateLabel( "Cancelled update." );
+                }
+                else {
+                    m_pWnd.Dispatcher.Invoke( (Action)( () => {
+                        m_pWnd.Close();
+                    } ) );
+                }
+            }
+            catch ( Exception ex ) {
+                MessageBox.Show( string.Format( "There was an error at Patch.PatchClient()\r\n{0}", ex.Message ), "Error!", MessageBoxButton.OK, MessageBoxImage.Error );
+            }
+        }
+
+        private void CheckForLauncherUpdate() {
+            string szDownloadBuffer;
+            using ( System.Net.WebClient wc = new System.Net.WebClient() ) {
+                szDownloadBuffer = wc.DownloadString( PATCH_SERVER + "version.txt" );
+
+                if ( Convert.ToInt32(szDownloadBuffer) > VERSION) {
+                    string szNewLauncherZipPath = "";
+                    wc.DownloadFile( string.Format( "{0}Launcher.zip", PATCH_SERVER ), szNewLauncherZipPath = Path.GetTempFileName() );
+
+                    File.Move( LAUNCHER_EXE, Path.Combine( ROOT_FOLDER, "Launcher.exe_OLD" ) );
+                    ExtractZipFile( szNewLauncherZipPath, null, ROOT_FOLDER );
+
+                    if ( File.Exists( szNewLauncherZipPath ) ) {
+                        File.Delete( szNewLauncherZipPath );
+                    }
+
+                    ProcessStartInfo Info = new ProcessStartInfo();
+                    Info.Arguments = "/C ping 127.0.0.1 -n 3 && \"" + LAUNCHER_EXE + "\"";
+                    Info.WindowStyle = ProcessWindowStyle.Hidden;
+                    Info.CreateNoWindow = true;
+                    Info.FileName = "cmd.exe";
+                    Process.Start( Info );
+                    m_pWnd.Dispatcher.Invoke( (Action)( () => {
+                        m_pWnd.Close();
+                    } ) );
+                }
+            }
+        }
+
+        private void CheckForUpdates() {
+            using ( System.Net.WebClient webClient = new System.Net.WebClient() ) {
+                string version = Encoding.ASCII.GetString( webClient.DownloadData( PATCH_SERVER + "maxversion.txt" ) );
+                string webData = Encoding.ASCII.GetString( webClient.DownloadData( PATCH_SERVER + "patchlist.txt" ) );
+                int.TryParse( version, out m_nNewVersion );
+
+                string[] patchList = webData.Split( '\n' );
+
+                foreach ( string s in patchList ) {
+                    string[] szTmp = s.Split( '\t' );
+                    CSPATCH_INDEX index;
+                    index.hashedName = szTmp[0];
+                    index.realName = szTmp[1];
+                    index.version = int.Parse( szTmp[2] );
+                    m_lPatchList.Add( index );
+                }
+
+                if ( CreatePatchList() ) {
+                    InvokeUpdateLabel( "Found update!" );
+                    PatchFiles();
+                    File.WriteAllText( Path.Combine( ROOT_FOLDER, "version" ), version );
+                }
+                else {
+                    m_pWnd.updateProgress.Dispatcher.Invoke((Action)(() =>
+                    {
+                        m_pWnd.updateProgress.IsIndeterminate = false;
+                    }));
+                    InvokeUpdateLabel( "Already updated." );
+                    InvokeUpdatePercentage( 100 );
+                }
+                InvokeStartButton();
+            }
+        }
+
+        private bool CreatePatchList() {
+            bool bResult = false;
+            List<CSPATCH_INDEX> newPatch = new List<CSPATCH_INDEX>();
+            foreach ( CSPATCH_INDEX index in m_lPatchList ) {
+                if ( index.version > GetVersion() && index.version <= m_nNewVersion ) {
+                    newPatch.Add( index );
+                    bResult = true;
+                }
+            }
+            m_lPatchList = newPatch;
+            return bResult;
+        }
+
+        private int GetVersion() {
+            if ( m_nCurrentVersion == 0 ) {
+                string szFile = Path.Combine( ROOT_FOLDER, "version" );
+                if ( File.Exists( szFile ) ) {
+                    string szContent = File.ReadAllText( szFile );
+                    if ( !int.TryParse( szContent, out m_nCurrentVersion ) ) {
+                        File.Delete( szFile );
+                        m_nCurrentVersion = 1;
+                    }
+                    return m_nCurrentVersion;
+                }
+                else {
+                    m_nCurrentVersion = 1;
+                    return m_nCurrentVersion;
+                }
+            }
+            else {
+                return m_nCurrentVersion;
+            }
+        }
+
+        private void PatchFiles() {
+            XClientManager manager = new XClientManager();
+            manager.Open( Path.Combine( ROOT_FOLDER, "data.000" ) );
+            m_pWnd.updateProgress.Dispatcher.Invoke((Action)(() =>
+            {
+                m_pWnd.updateProgress.IsIndeterminate = false;
+                m_pWnd.updateProgress.Maximum = m_lPatchList.Count;
+            }));
+
+            foreach ( CSPATCH_INDEX index in m_lPatchList ) {
+                InvokeUpdateLabel( "Currently updating: " + index.hashedName );
+                string szZip = "";
+                string szTemp = Path.Combine( Path.GetTempPath(), index.realName );
+
+                using ( var webClient = new System.Net.WebClient() ) {
+                    string szDownloadURL = string.Format( "{0}/files/{2}.zip", PATCH_SERVER, index.version, index.realName );
+                    webClient.DownloadFile( szDownloadURL, ( szZip = Path.GetTempFileName() ) );
+                    if ( File.Exists( szTemp ) )
+                        File.Delete( szTemp );
+
+                    ExtractZipFile( szZip, null, Path.GetTempPath() );
+
+                }
+                //if(new System.IO.FileInfo(szTemp).Length > 15728640 ) // MB
+                manager.Patch( szTemp, ROOT_FOLDER, index );
+                if ( File.Exists( szTemp ) )
+                    File.Delete( szTemp );
+
+                InvokeUpdatePercentage(1);
+            }
+            manager.Save( Path.Combine( ROOT_FOLDER, "data.000" ) );
+
+            InvokeUpdatePercentage( 100 );
+            InvokeUpdateLabel( "Update successful." );
+        }
+
+        #region Invokes
+
+        private void InvokeUpdateLabel(string szContent)
+        {
+            m_pWnd.lbUpdates.Dispatcher.Invoke((Action)(() =>
+            {
+                m_pWnd.lbUpdates.Content = szContent;
+            }));
+        }
+
+        private void InvokeUpdatePercentage(int percentage)
+        {
+            int strPercentage = 100;
+            m_pWnd.updateProgress.Dispatcher.Invoke((Action)(() =>
+            {
+                if (percentage == 100)
+                    m_pWnd.updateProgress.Value = 100;
+                else
+                {
+                    m_pWnd.updateProgress.Value += percentage;
+                    strPercentage = (int)Math.Round((m_pWnd.updateProgress.Value / m_pWnd.updateProgress.Maximum) * 100);
+                }
+            }));
+
+            m_pWnd.lbUpdatePercentage.Dispatcher.Invoke((Action)(() =>
+            {
+                m_pWnd.lbUpdatePercentage.Content = strPercentage.ToString() + "%";
+            }));
+        }
+
+        private void InvokeStartButton()
+        {
+
+            m_pWnd.btnStart.Dispatcher.Invoke((Action)(() =>
+            {
+                var uri = new Uri("pack://application:,,,/play_on.png");
+                System.Windows.Media.ImageSource img = new System.Windows.Media.Imaging.BitmapImage(uri); // set the ImageSource property
+                m_pWnd.btnStart.Source = img;
+            }));
+            MainWindow.isStartable = true;
+        }
+
+        #endregion
+
+        public void ExtractZipFile(string archiveFilenameIn, string password, string outFolder)
+        {
+            FastZip fastZip = new FastZip();
+            string fileFilter = null;
+
+            // Will always overwrite if target filenames already exist
+            fastZip.ExtractZip(archiveFilenameIn, outFolder, fileFilter);
+        }
+    }
+
+    public struct CSPATCH_INDEX
+    {
+        public string hashedName;
+        public string realName;
+        public int version;
+    };
+}
